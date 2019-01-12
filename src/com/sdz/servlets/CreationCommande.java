38a85071900e3399c895d123d55e765992b0024c
Justine <justine.denoeud@gmail.com>
2019-01-12 12:03:34 +0100

    Client and Commande pages are working properly

--- src/com/sdz/servlets/CreationCommande.java
+++ src/com/sdz/servlets/CreationCommande.java
@@ -1,11 +1,20 @@
 package com.sdz.servlets;
 
 import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import com.sdz.beans.Client;
+import com.sdz.beans.Commande;
 
 /**
  * Servlet implementation class CreationCommande
@@ -23,7 +32,76 @@ public class CreationCommande extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		this.getServletContext().getRequestDispatcher( "/WEB-INF/creerCommande.jsp" ).forward( request, response );
+		/*
+         * Récupération des données saisies, envoyées en tant que paramètres de
+         * la requête GET générée à la validation du formulaire
+         */
+        String nom = request.getParameter( "nomClient" );
+        String prenom = request.getParameter( "prenomClient" );
+        String adresse = request.getParameter( "adresseClient" );
+        String telephone = request.getParameter( "telephoneClient" );
+        String email = request.getParameter( "emailClient" );
+
+        /* Récupération de la date courante */
+
+        DateTime dt = new DateTime();
+
+        /* Conversion de la date en String selon le format défini */
+
+        org.joda.time.format.DateTimeFormatter fmt = DateTimeFormat.forPattern( "dd/MM/yyyy HH:mm:ss" );
+        String date = dt.toString(fmt);
+        
+        double montant;
+        try {
+            /* Récupération du montant */
+            montant = Double.parseDouble( request.getParameter( "montantCommande" ) );
+        } catch ( NumberFormatException e ) {
+            /* Initialisation à -1 si le montant n'est pas un nombre correct */
+            montant = -1;
+        }
+        String modePaiement = request.getParameter( "modePaiementCommande" );
+        String statutPaiement = request.getParameter( "statutPaiementCommande" );
+        String modeLivraison = request.getParameter( "modeLivraisonCommande" );
+        String statutLivraison = request.getParameter( "statutLivraisonCommande" );
+
+        String message;
+        /*
+         * Initialisation du message à afficher : si un des champs obligatoires
+         * du formulaire n'est pas renseigné, alors on affiche un message
+         * d'erreur, sinon on affiche un message de succès
+         */
+        if ( nom.trim().isEmpty() || adresse.trim().isEmpty() || telephone.trim().isEmpty() || montant == -1
+                || modePaiement.isEmpty() || modeLivraison.isEmpty() ) {
+            message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"creerCommande.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'une commande.";
+        } else {
+            message = "Commande créée avec succès !";
+        }
+        /*
+         * Création des beans Client et Commande et initialisation avec les
+         * données récupérées
+         */
+        Client client = new Client();
+        client.setNom( nom );
+        client.setPrenom( prenom );
+        client.setAdresse( adresse );
+        client.setTelephone( telephone );
+        client.setEmail( email );
+
+        Commande commande = new Commande();
+        commande.setClient( client );
+        commande.setDate( date );
+        commande.setMontant( montant );
+        commande.setModePaiement( modePaiement );
+        commande.setStatutPaiement( statutPaiement );
+        commande.setModeLivraison( modeLivraison );
+        commande.setStatutLivraison( statutLivraison );
+
+        /* Ajout du bean et du message à l'objet requête */
+        request.setAttribute( "commande", commande );
+        request.setAttribute( "message", message );
+
+        /* Transmission à la page JSP en charge de l'affichage des données */
+        this.getServletContext().getRequestDispatcher( "/afficherCommande.jsp" ).forward( request, response );
 	}
 
 
