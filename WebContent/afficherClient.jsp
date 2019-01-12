fa70f61a911d88b3d4a7c68884a4b2230768e986
Justine <justine.denoeud@gmail.com>
2019-01-12 22:00:43 +0100

    Tp part2 realized for client

--- WebContent/afficherClient.jsp
+++ WebContent/afficherClient.jsp
@@ -10,14 +10,19 @@
 <body>
 
 	<div> <c:import url="inc/menu.jsp"/> </div>
-
-	<p class="info"> ${ message } </p>
 	
-	<p>Client</p>
-	<p>Nom: <c:out value="${ client.nom }" /> </p>
-	<p>Prénom : <c:out value="${ client.prenom }" /></p>
-    <p>Adresse : <c:out value="${ client.adresse }"/></p>
-    <p>Numéro de téléphone : <c:out value="${ client.telephone }"/></p>
-    <p>Email : <c:out value="${ client.email }"/></p>
+	<c:choose>
+		<c:when test="${!erreur}">
+			<p class="info">Client créé avec succès !</p>
+			<p>Nom: <c:out value="${ client.nom }" /> </p>
+			<p>Prénom : <c:out value="${ client.prenom }" /></p>
+		    <p>Adresse : <c:out value="${ client.adresse }"/></p>
+		    <p>Numéro de téléphone : <c:out value="${ client.telephone }"/></p>
+		    <p>Email : <c:out value="${ client.email }"/></p>
+	    </c:when>
+	    <c:otherwise>
+	    	<p class="info"> ${ message } </p>
+	    </c:otherwise>
+    </c:choose>
 </body>
 </html>
--- src/com/sdz/servlets/CreationClient.java
+++ src/com/sdz/servlets/CreationClient.java
@@ -15,7 +15,18 @@ import com.sdz.beans.Client;
 
 public class CreationClient extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	public static final String VUE="/afficherClient.jsp";
+	
+	public static final String ATT_CLIENT="client";
+	public static final String ATT_MESSAGE="message";
+	public static final String ATT_ERREUR="erreur";
+	
+	public static final String CHAMP_NOM       = "nomClient";
+    public static final String CHAMP_PRENOM    = "prenomClient";
+    public static final String CHAMP_ADRESSE   = "adresseClient";
+    public static final String CHAMP_TELEPHONE = "telephoneClient";
+    public static final String CHAMP_EMAIL     = "emailClient";
+	
 
     public CreationClient() {
         super();
@@ -27,19 +38,23 @@ public class CreationClient extends HttpServlet {
 		// TODO Auto-generated method stub
 		
 		//Récupération des paramètres de la requete
-		String nom= request.getParameter("nomClient");
-		String prenom = request.getParameter( "prenomClient" );
-        String adresse = request.getParameter( "adresseClient" );
-        String telephone = request.getParameter( "telephoneClient" );
-        String email = request.getParameter( "emailClient" );
+		String nom = request.getParameter( CHAMP_NOM );
+	    String prenom = request.getParameter( CHAMP_PRENOM );
+	    String adresse = request.getParameter( CHAMP_ADRESSE );
+	    String telephone = request.getParameter( CHAMP_TELEPHONE );
+	    String email = request.getParameter( CHAMP_EMAIL );
+	        
         String message ;
+        boolean erreur;
         
         //vérification des paramètres rentrés
         if(nom.isEmpty() || adresse.isEmpty() || telephone.isEmpty()) {
         	message = "Veuillez remplir tous les champs oligatoires!"
         			+ " <br> <a href=\"creerClient.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'un client. ";
+        	erreur = true;
         } else {
         	message = "Client créé avec succès";
+        	erreur = false;
         }
 		
 		//Création du client et initialisation des paramètres
@@ -51,10 +66,11 @@ public class CreationClient extends HttpServlet {
 	    client.setTelephone( telephone );
 	    client.setEmail( email );
 		
-	    request.setAttribute("client", client);
-	    request.setAttribute("message", message);
+	    request.setAttribute(ATT_CLIENT, client);
+	    request.setAttribute(ATT_MESSAGE, message);
+	    request.setAttribute(ATT_ERREUR, erreur);
 		
-		this.getServletContext().getRequestDispatcher( "/afficherClient.jsp" ).forward( request, response );
+		this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
 		
 	}
 
