bf3198b2ad084d7afa85ed241ab392b56877d49b
DENOEUD <justine.denoeud@capgemini.com>
2019-01-29 13:38:27 +0100

    Add Dao design pattern

---
+++ src/com/sdz/dao/DAOConfigurationException.java
@@ -0,0 +1,18 @@
+package com.sdz.dao;
+
+public class DAOConfigurationException extends RuntimeException {
+    /*
+     * Constructeurs
+     */
+    public DAOConfigurationException( String message ) {
+        super( message );
+    }
+
+    public DAOConfigurationException( String message, Throwable cause ) {
+        super( message, cause );
+    }
+
+    public DAOConfigurationException( Throwable cause ) {
+        super( cause );
+    }
+}
---
+++ src/com/sdz/dao/UtilisateurDao.java
@@ -0,0 +1,11 @@
+package com.sdz.dao;
+
+import com.sdz.beans.Utilisateur;
+
+public interface UtilisateurDao {
+
+    void creer( Utilisateur utilisateur ) throws DAOException;
+
+    Utilisateur trouver( String email ) throws DAOException;
+
+}
